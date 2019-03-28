7022c9a80076137b92ccba29e55ad9cf70b60cf2
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-03-28 01:20:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates :title, inclusion: {in: ["Won't Believe", "Secret", "Top [number]", "Guess"]}
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
