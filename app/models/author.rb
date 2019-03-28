6c6777aa67a8184b6723a4c46fa574ba6df77fe0
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-03-28 01:05:51 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  validates :name, presence: true, uniquenss: true
+  validates :name, presence: true, uniqueness: true
   validates :phone_number, length: {minimum: 10}
   
 end
